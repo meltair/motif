2a6565bc242bb374b172f1d33e85f0da5e353ba6
Mehmet Aktaş <mehmet.aktas@turksat.com.tr>
2026-03-30 16:20:16 +0300

    proje klasor refactor

--- components/MotifWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import "@motif-ui/react/themes/default-theme.css";
-import { MotifProvider } from "@motif-ui/react";
-
-export function MotifWrapper({ children }: { children: React.ReactNode }) {
-    return <MotifProvider>{children}</MotifProvider>;
-}
